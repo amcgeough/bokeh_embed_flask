73791620ee4c2bcb38c3ddaf890ecb83f77f982d
amcgeough <andrewmcgeough@Andrews-MacBook-Pro.local>
2017-10-15 12:42:41 +0100

    serve

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ app = Flask(__name__)
 
 subprocess.Popen(
 #    ['bokeh', 'serve','--allow-websocket-origin=127.0.0.1:5000', '--port=5000','bokeh_plot.py'], stdout=subprocess.PIPE)
-    ['bokeh', 'serve','bokeh_plot.py'], stdout=subprocess.PIPE)
+    #['bokeh', 'serve','bokeh_plot.py'], stdout=subprocess.PIPE)
+    ['bokeh-server', '--port 8080', '--ip 127.0.0.1', '--backend memory']
 
 """
 @atexit.register
